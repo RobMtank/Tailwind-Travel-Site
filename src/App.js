8862827e78fefc5793c38f016918ab4660bec834
RobMtank <siddhantydhyani99@gmail.com>
2023-02-02 11:31:19 +0530

    components updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Hero from './components/hero/Hero';
-import Navbar from './components/navbar/Navbar';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
